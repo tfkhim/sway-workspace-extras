383039a0e30285560c721592856688c71a01caac
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-11-25 10:35:51 +0100

    test: add tests for shift_successors

---
+++ tests/shift_successors.rs
@@ -0,0 +1,132 @@
+/*
+ * This file is part of sway-workspace-extras
+ *
+ * Copyright (c) 2022 Thomas Himmelstoss
+ *
+ * This software is subject to the MIT license. You should have
+ * received a copy of the license along with this program.
+ */
+
+mod node_builder;
+
+use node_builder::{single_output, Node};
+use sway_workspace_extras::{Action, Workflow};
+
+#[test]
+fn single_empty_workspace() {
+    let tree = single_output(|output| {
+        output.workspace(1).focused();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(actions, &[]);
+}
+
+#[test]
+fn single_workspace_with_one_window() {
+    let tree = single_output(|output| {
+        output.workspace(1).add_focused_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(actions, &[]);
+}
+
+#[test]
+fn current_workspace_is_empty() {
+    let tree = single_output(|output| {
+        output.workspace(1).focused();
+        output.workspace(2).add_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[Action::RenameWorkspace {
+            workspace_num: 2,
+            new_workspace_num: 3
+        }]
+    );
+}
+
+#[test]
+fn current_workspace_not_empty() {
+    let tree = single_output(|output| {
+        output.workspace(1).add_focused_window();
+        output.workspace(2).add_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[Action::RenameWorkspace {
+            workspace_num: 2,
+            new_workspace_num: 3
+        }]
+    );
+}
+
+#[test]
+fn next_workspace_is_empty() {
+    let tree = single_output(|output| {
+        output.workspace(1).add_focused_window();
+        output.workspace(3).add_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(actions, &[]);
+}
+
+#[test]
+fn gap_between_successors() {
+    let tree = single_output(|output| {
+        output.workspace(1).add_focused_window();
+        output.workspace(2).add_window();
+        output.workspace(4).add_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[Action::RenameWorkspace {
+            workspace_num: 2,
+            new_workspace_num: 3
+        }]
+    );
+}
+
+#[test]
+fn multiple_successors() {
+    let tree = single_output(|output| {
+        output.workspace(1).add_focused_window();
+        output.workspace(2).add_window();
+        output.workspace(3).add_window();
+    });
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[
+            Action::RenameWorkspace {
+                workspace_num: 3,
+                new_workspace_num: 4
+            },
+            Action::RenameWorkspace {
+                workspace_num: 2,
+                new_workspace_num: 3
+            }
+        ]
+    );
+}
+
+fn when_shift_successors(tree: Node) -> Vec<Action> {
+    let workflow = Workflow::new(&tree).unwrap();
+    workflow.shift_successors()
+}
