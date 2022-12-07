6052dff8de09931871297a43a3d8bcfc3e632a8f
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-12-07 21:23:42 +0100

    test: add test for the shift_successors command
    The command didn't have any tests with multiple outputs.

--- tests/shift_successors.rs
+++ tests/shift_successors.rs
@@ -9,7 +9,7 @@
 
 mod node_builder;
 
-use node_builder::{single_output, Node};
+use node_builder::{single_output, two_outputs, Node};
 use sway_workspace_extras::{Action, Workflow};
 
 #[test]
@@ -124,6 +124,112 @@ fn multiple_successors() {
             }
         ]
     );
+}
+
+#[test]
+fn two_empty_outputs() {
+    let tree = two_outputs(
+        |output_1| {
+            output_1.workspace(1).focused();
+        },
+        |output_2| {
+            output_2.workspace(2);
+        },
+    );
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[Action::RenameWorkspace {
+            workspace_num: 2,
+            new_workspace_num: 3
+        }]
+    );
+}
+
+#[test]
+fn on_second_output_without_successors() {
+    let tree = two_outputs(
+        |output_1| {
+            output_1.workspace(1);
+        },
+        |output_2| {
+            output_2.workspace(2).focused();
+        },
+    );
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(actions, &[]);
+}
+
+#[test]
+fn gap_between_successors_on_different_outputs() {
+    let tree = two_outputs(
+        |output_1| {
+            output_1.workspace(1).focused();
+            output_1.workspace(2).add_window();
+        },
+        |output_2| {
+            output_2.workspace(4);
+        },
+    );
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[Action::RenameWorkspace {
+            workspace_num: 2,
+            new_workspace_num: 3
+        }]
+    );
+}
+
+#[test]
+fn successors_on_different_outputs() {
+    let tree = two_outputs(
+        |output_1| {
+            output_1.workspace(1).focused();
+            output_1.workspace(2).add_window();
+        },
+        |output_2| {
+            output_2.workspace(3);
+        },
+    );
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(
+        actions,
+        &[
+            Action::RenameWorkspace {
+                workspace_num: 3,
+                new_workspace_num: 4
+            },
+            Action::RenameWorkspace {
+                workspace_num: 2,
+                new_workspace_num: 3
+            }
+        ]
+    );
+}
+
+#[test]
+fn two_workspace_with_gap_between() {
+    let tree = two_outputs(
+        |output_1| {
+            output_1.workspace(1).add_focused_window();
+        },
+        |output_2| {
+            output_2.workspace(3);
+        },
+    );
+
+    let actions = when_shift_successors(tree);
+
+    assert_eq!(actions, &[]);
 }
 
 fn when_shift_successors(tree: Node) -> Vec<Action> {
